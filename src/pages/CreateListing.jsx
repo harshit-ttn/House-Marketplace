2e15f44122035b9253c0898d55de5aa561094e05
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-19 22:47:12 +0530

    geolocation api key

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -88,7 +88,9 @@ function CreateListing() {
     let location
 
     if (geolocationEnabled) {
-      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
+      // const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
+
+      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}`)
 
       const data = await response.json()
 
